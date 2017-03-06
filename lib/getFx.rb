300c9b023d42dbbebe5c1eb4577ff38badbd84d7
Chris Quinn <chris@gravitywell.co.uk>
2017-03-06 22:34:11 +0000

    adding basic conversion multipler

--- lib/getFx.rb
+++ lib/getFx.rb
@@ -1,23 +1,25 @@
 require 'nokogiri'
 
 class GetFx
-  def self.run(curr = "GBP", time = "2017-02-28")
-    parser = Parser.new(curr, time)
+  def self.run(curr = "GBP", time = "2017-02-28", amt = 1)
+    parser = Parser.new(curr, time, amt)
     parser.run
   end
 end
 
 class GetFx::Parser
-  def initialize(curr, time)
+  def initialize(curr, time, amt)
     @curr = curr
     @date = time
+    @amt = amt.to_f
     @doc = File.open("feed.xml") { |f| Nokogiri::XML(f) }
   end
 
   def run
     if(@curr && @date)
-      puts 'fetching ' + @curr + ' on ' + @date
       r = @doc.css("[@time='" + @date + "'] > [@currency='" + @curr + "']")[0]["rate"]
+      i = r.to_f
+      i * @amt
     end
   end
 end
