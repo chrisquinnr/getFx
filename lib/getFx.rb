edc3f4b1aa2890fcf0b60434842af0c7512165de
Chris Quinn <chris@gravitywell.co.uk>
2017-03-08 22:57:48 +0000

    validation and logic reworked a little and tests amended inline.

--- lib/getFx.rb
+++ lib/getFx.rb
@@ -1,28 +1,43 @@
 require 'nokogiri'
 
 class GetFx
-  def self.run(*opts)
-    opts[0] ? curr = opts[0] : curr = "USD"
-    opts[1] ? time = opts[1] : time = "2017-02-28"
-    opts[2] ? amt = opts[2] : amt = 1
-    parser = Parser.new(curr, time, amt)
+  def self.run(*params)
+    if params
+      args = params[0]
+    end
+
+    curr = "USD"
+    if args[0]
+      curr = args[0]
+    end 
+
+    date = "2017-02-28"
+    if args[1]
+      date = args[1]
+    end
+
+    amt = 1
+    if args[2]
+      amt = args[2]
+    end
+
+    parser = Parser.new(curr, date, amt)
     parser.run
+
   end
 end
 
 class GetFx::Parser
-  def initialize(curr, time, amt)
-    @curr = curr
-    @date = time
-    @amt = amt.to_f
+  def initialize(curr, date, amt)
     @doc = File.open("feed.xml") { |f| Nokogiri::XML(f) }
+    @curr = curr
+    @date = date
+    @amt = amt.to_f
   end
 
   def run
-    if(@curr && @date && @amt)
-      r = @doc.css("[@time='" + @date + "'] > [@currency='" + @curr + "']")[0]["rate"]
-      i = r.to_f
-      i * @amt
-    end
+    r = @doc.css("[@time='" + @date + "'] > [@currency='" + @curr + "']")[0]["rate"]
+    i = r.to_f
+    i * @amt
   end
 end
--- test/test_getFx.rb
+++ test/test_getFx.rb
@@ -4,21 +4,21 @@ require 'getFx'
 class GetFxTest < Minitest::Test
   def test_run_defaults
     assert_equal 1.0597,
-      GetFx.run()
+      GetFx.run([])
   end
 
   def test_run_currency
     assert_equal 0.85305,
-      GetFx.run('GBP')
+      GetFx.run(['GBP'])
   end
 
   def test_run_currency_date
     assert_equal 0.8555,
-      GetFx.run('GBP', '2017-03-01')
+      GetFx.run(['GBP', '2017-03-01'])
   end
 
   def test_run_currency_date_amt
     assert_equal 1.711,
-      GetFx.run('GBP', '2017-03-01', 2)
+      GetFx.run(['GBP', '2017-03-01', 2])
   end
 end
